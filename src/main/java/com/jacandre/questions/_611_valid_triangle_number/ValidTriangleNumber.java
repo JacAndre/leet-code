b973255e5504658949dbd5989cc914708a0e6ca4
Jacques Andre <jacques_andre@ymail.com>
2025-09-27 12:02:10 +0100

    Solution to 26/09/25 daily problem

---
+++ src/main/java/com/jacandre/questions/_611_valid_triangle_number/ValidTriangleNumber.java
@@ -0,0 +1,60 @@
+package com.jacandre.questions._611_valid_triangle_number;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given an integer array nums, return the number of triplets chosen from the array that can make triangles if we take them as side lengths of a triangle.
+ */
+
+public class ValidTriangleNumber {
+    public int triangleNumber(int[] nums) {
+        var COMBINATION_SIZE = 3;
+        List<Integer> input = Arrays.stream(nums).boxed().toList();
+
+        // Generate all 3 element combinations from a list of inputs
+        var generatedCombinations = generateCombinations(input, COMBINATION_SIZE);
+        // Filter out combinations that do not satisfy triangle requirements
+        var validCombinations = validateTripletCombinations(generatedCombinations);
+
+        return validCombinations.size();
+    }
+
+    private static <Integer> List<List<Integer>> generateCombinations(List<Integer> input, int combinationSize) {
+        List<List<Integer>> result = new ArrayList<>();
+        buildCombinations(input, combinationSize, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static <T> void buildCombinations(
+            List<T> input,
+            int combinationSize,
+            int startIndex,
+            List<T> currentCombination,
+            List<List<T>> result
+    ) {
+        if (currentCombination.size() == combinationSize) {
+            result.add(new ArrayList<>(currentCombination));
+        } else {
+            for (int i = startIndex; i < input.size(); i++) {
+                currentCombination.add(input.get(i));
+                buildCombinations(input, combinationSize, i + 1, currentCombination, result);
+                currentCombination.removeLast();
+            }
+        }
+    }
+
+    private static List<List<Integer>> validateTripletCombinations(List<List<Integer>> generatedCombinations) {
+        List<List<Integer>> validTripletCombinations = new ArrayList<>();
+        for (List<Integer> triplet : generatedCombinations) {
+            List<Integer> sortedList = triplet.stream().sorted().toList();
+            // A valid triangle is one where the sum of the two shortest sides is longer than the longest side
+            if (sortedList.get(0) + sortedList.get(1) > sortedList.get(2)) {
+                validTripletCombinations.add(sortedList);
+            }
+        }
+
+        return validTripletCombinations;
+    }
+}
---
+++ src/test/java/com/jacandre/questions/ValidTriangleNumberTest.java
@@ -0,0 +1,18 @@
+package com.jacandre.questions;
+
+import com.jacandre.questions._611_valid_triangle_number.ValidTriangleNumber;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ValidTriangleNumberTest {
+    @Test
+    void testTriangleNumber() {
+        int[] nums = {2, 2, 3, 4};
+        ValidTriangleNumber solver = new ValidTriangleNumber();
+        assertEquals(3, solver.triangleNumber(nums));
+    }
+}
